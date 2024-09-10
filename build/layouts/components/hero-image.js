fdff31aa78554407d4e5bb693d989e8d74eef83b
Amir Daneshmand <amirdanesh02@gmail.com>
2024-09-10 17:37:54 +0330

    Test commit

--- build/layouts/components/hero-image.js
+++ build/layouts/components/hero-image.js
@@ -1,19 +1,26 @@
 import { getDiv, getHeading } from "../utils/elements.js";
+/* Main builder function */
 const buildHeroImage = (parent) => {
     const container = getDiv();
     container.className = "hero-image";
+    // Attach hero text to the bottom
+    // of the following container
     buildHeroText(container);
     parent.append(container);
 };
+/* Builds and applies hero text component to the hero image */
 const buildHeroText = (parent) => {
+    // Just a set of the headings
     const captions = [
         { level: 3, text: "вас" },
         { level: 2, text: "вітає" },
         { level: 1, text: "Карамеїв" },
     ];
+    // Start building process
     const heroText = getDiv();
     heroText.className = "hero-text";
-    heroText.classList.add("swing");
+    heroText.classList.add("swing"); // Enables up and down moving animation
+    // Process each of headings text
     captions.forEach((caption) => {
         const heading = getHeading(caption.level);
         heading.innerHTML = caption.text;
@@ -21,6 +28,7 @@ const buildHeroText = (parent) => {
     });
     parent.append(heroText);
 };
+/* Starter function for external use */
 export default function applyHeroImage(parent) {
     buildHeroImage(parent);
 }
--- build/layouts/components/text-line.js
+++ build/layouts/components/text-line.js
@@ -1,7 +1,9 @@
 import { getDiv, getParagraph } from "../utils/elements.js";
+/* Main builder function */
 const buildLine = (parent, props) => {
     let line = getDiv();
     line.className = "text-line";
+    // If background color or image is provided - apply them,
     if (props.color) {
         line.style.backgroundColor = props.color;
     }
@@ -10,29 +12,38 @@ const buildLine = (parent, props) => {
         line.style.backgroundImage = props.image;
     }
     else {
+        // Otherwise, apply white background as the default
         line.style.backgroundColor = "#fff";
     }
     if (props.textColor) {
         line.style.color = props.textColor;
     }
+    // Attach provided text
     buildText(line, props.text);
     parent.append(line);
 };
+/* Operates on both arrays of text and on the single text lines.    *
+ * In the case of arrays, it uses alternative "buildTexts" function */
 const buildText = (parent, props) => {
     let paragraph = getParagraph();
-    paragraph.className = "single-text";
+    // If the source text is presented as array of
+    // texts - operate it accordingly to array
     if (typeof props === "object") {
         buildTexts(parent, props);
     }
     else {
+        // Otherwise, just apply it and classify it as single text line
         paragraph.innerHTML = props;
+        paragraph.className = "single-text";
     }
     parent.append(paragraph);
 };
+/* Operates on text represented as array of strings */
 const buildTexts = (parent, props) => {
-    let textContainer = getDiv();
-    let texts = props;
+    let textContainer = getDiv(); // Main element
+    let texts = props; // Strings to be processed
     textContainer.className = "text-group";
+    // Process each string and apply to the main element
     texts.map((text) => {
         const paragraph = getParagraph();
         paragraph.innerHTML = text;
@@ -40,6 +51,7 @@ const buildTexts = (parent, props) => {
     });
     parent.append(textContainer);
 };
+/* Starter function for external use */
 export default function applyTextLine(parent, props) {
     buildLine(parent, props);
 }
